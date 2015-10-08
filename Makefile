0c6f407a262190b297a476e2a26b8c58134b7cb1
Marc Beillevaire <marc.beillevaire@gmail.com>
2015-10-08 14:56:45 +0200

    Now recording 1000 obs and training over it -> 233pts

--- Makefile
+++ Makefile
@@ -9,14 +9,33 @@ FIFO = player2server
 
 INPUTFILE = SouthEmissions
 
-all: $(EXE)
+exe: $(EXE)
 
 $(EXE): $(SRC)
 	$(JC) $(JFLAGS) $^ 
 
-game:
+game: fifo
+	java Main server < $(FIFO) | java Main verbose > $(FIFO)
+
+north: fifo
+	java Main load NorthEmissions.in server < $(FIFO) | java Main load NorthEmissionsOpponent.in verbose > $(FIFO)
+
+south: fifo
+	java Main load SouthEmissions.in server < $(FIFO) | java Main load SouthEmissionsOpponent.in verbose > $(FIFO)
+
+west: fifo
+	java Main load WestEmissions.in server < $(FIFO) | java Main load WestEmissionsOpponent.in verbose > $(FIFO)
+
+east: fifo
+	java Main load EastEmissions.in server < $(FIFO) | java Main load EastEmissionsOpponent.in verbose > $(FIFO)
+
+paradise: fifo
+	java Main load ParadiseEmissions.in server < $(FIFO) | java Main load ParadiseEmissionsOpponent.in verbose > $(FIFO)
+
+all: north south west east paradise
+
+fifo:
 	rm -f $(FIFO) && mkfifo $(FIFO)
-	java Main load $(INPUTFILE).in server < $(FIFO) | java Main load $(INPUTFILE)Opponent.in verbose > $(FIFO)
 
 clean:
 	rm -f $(EXE)
--- Player.java
+++ Player.java
@@ -4,10 +4,16 @@ import java.util.ArrayList;
 
 class Player {
 
-    public static ArrayList<HMM> roundHmms;
-    public static HMM[] speciesHmm = new HMM[6];
-    public static int mvmt = 0;
-    public static boolean[] speciesSeen = new boolean[] {false, false, false, false, false, false};
+    private static final int NBR_HMM = 1;
+    private static final int NBR_OBS = 1000;
+
+    public ArrayList<HMM> roundHmms;
+    public HMM[][] speciesHmm = new HMM[6][NBR_HMM];
+    public int mvmt = 0;
+    public boolean[] speciesSeen = new boolean[] {false, false, false, false, false, false};
+
+    public int[][] obsSpecies = new int[6][NBR_OBS];
+    public int[] obsSpeciesSize = new int[] {0, 0, 0, 0, 0, 0};
 
     public Player() {
     }
@@ -39,34 +45,13 @@ class Player {
 
             ArrayList<HMM> hmms = new ArrayList<HMM>();
 
-            // Init HMM for each bird :
+            // Init new HMM randomly for each bird :
             for(int i = 0 ; i < pState.getNumBirds() ; i++) {
-                double[][] A_init = randStochMat(5,5);
-                double[][] B_init = randStochMat(5,9);
-                double[][] pi_init_2D = randStochMat(1,5);
-                double[] pi_init = new double[5];
-                for(int j = 0 ; j < 5 ; j++) {
-                    pi_init[j] = pi_init_2D[0][j];
-                }
-
-                HMM h = new HMM(A_init, B_init, pi_init);
+                HMM h = randHMM();
                 hmms.add(h);
             }
 
             roundHmms = new ArrayList<HMM>(hmms);
-        }
-
-        if(mvmt > 99) {
-            //for(int i = 0 ; i < pState.getNumBirds() ; i++) {
-            //    // Get the sequence of observations
-            //    int[] bSeq = new int[mvmt + 1];
-            //    for(int j = 0 ; j < bSeq.length ; j++) {
-            //        bSeq[j] = pState.getBird(i).getObservation(j);
-            //    }
-            //    roundHmms.get(i).estimateModel(bSeq);
-            //    Player.smoothStoch(roundHmms.get(i).A);
-            //    Player.smoothStoch(roundHmms.get(i).B);
-            //}
         }
 
         mvmt++;
@@ -94,8 +79,9 @@ class Player {
         int[] lGuess = new int[pState.getNumBirds()];
 
         if(pState.getRound() == 0) {
+            Random randomGenerator = new Random();
             for (int i = 0; i < pState.getNumBirds(); ++i) {
-                lGuess[i] = Constants.SPECIES_PIGEON;
+                lGuess[i] = randomGenerator.nextInt(6);
             }
         } else {
             for (int i = 0; i < pState.getNumBirds(); ++i) {
@@ -107,19 +93,68 @@ class Player {
                 for(int j = 0 ; j < bSeq.length ; j++) {
                     bSeq[j] = pState.getBird(i).getObservation(j);
                 }
-                roundHmms.get(i).estimateModel(bSeq);
                 Player.smoothStoch(roundHmms.get(i).A);
                 Player.smoothStoch(roundHmms.get(i).B);
+                roundHmms.get(i).estimateModel(bSeq);
 
-                HMM birdHmm = Player.roundHmms.get(i);
-                double proba = -1, maxProba = -1;
+                HMM birdHmm = roundHmms.get(i);
+                double[] proba = new double[NBR_HMM];
+                double pr;
+                double maxProba = -1;
                 int mostProbSp = -1;
 
                 for(int k = 0 ; k < 6 ; k++) {
                     if(speciesSeen[k]) {
-                        proba = (speciesHmm[k]).estimateProbabilityOfEmissionSequence(bSeq);
-                        if(proba > maxProba) {
-                            maxProba = proba;
+                        // Getting all the probabilities of all hmm from this species
+                        for(int l = 0 ; l < NBR_HMM ; l++) {
+                            proba[l] = (speciesHmm[k][l]).estimateProbabilityOfEmissionSequence(bSeq);
+                        }
+                        // Getting the best probas using : mean -> 97 on kattis
+                        pr = 0;
+                        for(int l = 0 ; l < NBR_HMM ; l++) {
+                            pr += proba[l];
+                        }
+                        pr /= NBR_HMM;
+
+                        // /!\
+                        // NB : NBR_NMM must be > 3 in the following selectors !!
+                        // /!\
+                        
+                        /*
+                        // Sorting probas (decr) in array, needed for median & mean best 3
+                        for(int idx1 = 0 ; idx1 < NBR_HMM - 1 ; idx1++) {
+                            double max = proba[idx1];
+                            int maxIdx = idx1;
+                            for(int idx2 = idx1 + 1 ; idx2 < NBR_HMM ; idx2++) {
+                                if(max < proba[idx2]) {
+                                    max = proba[idx2];
+                                    maxIdx = idx2;
+                                }
+                            }
+                            double c = proba[maxIdx];
+                            proba[maxIdx] = proba[idx1];
+                            proba[idx1] = c;
+                        }
+                        // Getting the best proba using : best median -> 77 on kattis
+                        //pr = proba[NBR_HMM / 2];
+
+                        // Getting the best probas using : mean -> 97 on kattis
+                        //pr = 0;
+                        //for(int l = 0 ; l < NBR_HMM ; l++) {
+                        //    pr += proba[l];
+                        //}
+                        //pr /= NBR_HMM;
+                        
+                        // Getting the best probas using : mean among 3 bests -> 75 on kattis
+                        pr = 0;
+                        for(int l = 0 ; l < 3 ; l++) {
+                            pr += proba[l];
+                        }
+                        pr /= NBR_HMM;
+                        */
+
+                        if(pr > maxProba) {
+                            maxProba = pr;
                             mostProbSp = k;
                         }
                     }
@@ -128,12 +163,6 @@ class Player {
                 lGuess[i] = mostProbSp;
             }
         }
-        System.err.println();
-        System.err.print("Round : " + pState.getRound() + ", guesses: ");
-        for(int i = 0 ; i < lGuess.length ; i++) {
-            System.err.print(lGuess[i] + " ");
-        }
-        System.err.println();
         return lGuess;
     }
 
@@ -159,115 +188,52 @@ class Player {
      */
     public void reveal(GameState pState, int[] pSpecies, Deadline pDue) {
 
-        System.err.print("Reveal:             ");
         for(int i = 0 ; i < pState.getNumBirds() ; i++) {
             if(pState.getBird(i).isDead()) {
                 continue;
             }
-            System.err.print(pSpecies[i] + " ");
-
+            // If new species : training new HMMs and store them
             if(!speciesSeen[pSpecies[i]]) {
                 speciesSeen[pSpecies[i]] = true;
-                speciesHmm[pSpecies[i]] = roundHmms.get(i);
-            } else {
-                // Reestimate model
-                int[] bSeq = new int[mvmt];
-                for(int j = 0 ; j < bSeq.length ; j++) {
-                    bSeq[j] = pState.getBird(i).getObservation(j);
+                for(int l = 0 ; l < NBR_HMM ; l++) {
+                    speciesHmm[pSpecies[i]][l] = randHMM();
                 }
-
-                // Clearing hmm with smoothing function & reestimate model
-                Player.smoothStoch(speciesHmm[pSpecies[i]].A);
-                Player.smoothStoch(speciesHmm[pSpecies[i]].B);
-
-                speciesHmm[pSpecies[i]].estimateModel(bSeq);
             }
-        }
-        System.err.println();
 
+            // Adding the observations to observations collection
+            int s = obsSpeciesSize[pSpecies[i]]; // current number of obs for this species
+            for(int j = 0 ; j < mvmt ; j++) {
+                if(s + j < NBR_OBS) {
+                    obsSpecies[pSpecies[i]][s + j] = pState.getBird(i).getObservation(j);
+                }
+            }
+            s += mvmt;
+            if(s < NBR_OBS) {
+                obsSpeciesSize[pSpecies[i]] += s;
+            } else {
+                obsSpeciesSize[pSpecies[i]] = NBR_OBS;
+            }
+
+            // Clearing hmm with smoothing function & reestimate model
+            for(int l = 0; l < NBR_HMM ; l++) {
+                Player.smoothStoch(speciesHmm[pSpecies[i]][l].A);
+                Player.smoothStoch(speciesHmm[pSpecies[i]][l].B);
+
+                speciesHmm[pSpecies[i]][l].estimateModel( obsSpecies[pSpecies[i]] );
+            }
+
+        }
         mvmt = 0;
     }
 
     public static void smoothStoch(double[][] M) {
-        double epsilon = 0.0001;
+        double epsilon = 0.00000001;
         for(int i = 0 ; i < M.length ; i++) {
             for(int j = 0 ; j < M[0].length ; j++) {
                 M[i][j] += epsilon;
                 M[i][j] /= (1 + M[0].length * epsilon);
             }
         }
-    }
-
-    // This function looks for the best permutation to make A1 and A2 closer
-    public static Permut permut(double[][] A1, double[][] A2) {
-
-        int[] p = new int[] {0,1,2,3,4};
-        int[] minP = new int[] {0,1,2,3,4};
-        double err = 0;
-        double minErr = 10000;
-        
-        // Lot of loops to find all the permutations
-        for(int i0 = 0 ; i0 < 5 ; i0++) {
-            p[0] = i0;
-        for(int i1 = 0 ; i1 < 5 ; i1++) {
-            if(p[0] == i1)
-                continue;
-            p[1] = i1;
-        for(int i2 = 0 ; i2 < 5 ; i2++) {
-            if(p[0] == i2 || p[1] == i2)
-                continue;
-            p[2] = i2;
-        for(int i3 = 0 ; i3 < 5 ; i3++) {
-            if(p[0] == i3 || p[1] == i3 || p[2] == i3)
-                continue;
-            p[3] = i3;
-        for(int i4 = 0 ; i4 < 5 ; i4++) {
-            if(p[0] == i4 || p[1] == i4 || p[2] == i4 || p[3] == i4)
-                continue;
-            p[4] = i4;
-
-            // Now we have the permutation, we can compute the error
-            err = 0;
-            for(int i = 0 ; i < 5 ; i++) {
-                for(int j = 0 ; j < 5 ; j++) {
-                    err += (A1[i][j] - A2[p[i]][p[j]]) * (A1[i][j] - A2[p[i]][p[j]]);
-                }
-            }
-
-            if(err < minErr) {
-                minErr = err;
-                for(int i = 0 ; i < 5 ; i++) {
-                    minP[i] = p[i];
-                }
-            }
-        }
-        }
-        }
-        }
-        }
-
-        // Now let's apply the permutation on A2
-        double[][] A2_permut = new double[5][5];
-        for(int i = 0 ; i < 5 ; i++) {
-            for(int j = 0 ; j < 5 ; j++) {
-                A2_permut[i][j] = A2[minP[i]][minP[j]];
-            }
-        }
-
-        Permut permut = new Permut(A2_permut, minErr);
-        return permut;
-    }
-
-    // Compute the mean of A1 & A2
-    public static double[][] meanMat(double[][] A1, double[][] A2) {
-        double[][] M = new double[5][5];
-        for(int i = 0 ; i < 5 ; i++) {
-            for(int j = 0 ; j < 5 ; j++) {
-                M[i][j] = (A1[i][j] + A2[i][j]) / 2;
-            }
-        }
-
-        return M;
     }
 
     // Just print the matrix
@@ -303,7 +269,10 @@ class Player {
         int sumNorm = 0;
         for(int i = 0 ; i < nL ; i++) {
             for(int j = 0 ; j < nR ; j++) {
-                M[i][j] = (double)randomGenerator.nextInt(100);
+                M[i][j] = (double)randomGenerator.nextInt(100) + 1;
+                if(i == j) {
+                    M[i][j] += 100.0;
+                }
                 sumNorm += M[i][j];
             }
             for(int j = 0 ; j < nR ; j++) {
@@ -330,19 +299,18 @@ class Player {
         }
     }
 
+    public static HMM randHMM() {
+        double[][] A_init = randStochMat(5,5);
+        double[][] B_init = randStochMat(5,9);
+        double[][] pi_init_2D = randStochMat(1,5);
+        double[] pi_init = new double[5];
+        for(int j = 0 ; j < 5 ; j++) {
+            pi_init[j] = pi_init_2D[0][j];
+        }
+
+        return new HMM(A_init, B_init, pi_init);
+    }
+
     public static final Action cDontShoot = new Action(-1, -1);
 
-    public static double[][][] As = new double[6][5][5];
-    public static double[][][] Bs = new double[6][5][9];
-    public static double[][] pis = new double[6][5];
-
-    private static class Permut {
-        double[][] M;
-        double err;
-
-        Permut(double[][] M, double err) {
-            this.M = M;
-            this.err = err;
-        }
-    }
 }
